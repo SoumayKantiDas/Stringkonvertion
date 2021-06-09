453563b0838b704cc042087eadfe8b91fae12d70
Soumay kanti Das <anikds2859@outlook.com>
2021-06-09 12:44:43 +0600

    Add project files.

---
+++ Stringkonvertion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stringkonvertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstName = "Anik";
+                string lastname = "Das";
+            Console.WriteLine("Full name= " + firstName + lastname);
+            string A;
+            A = firstName + lastname;
+            Console.WriteLine(A);
+            A = A.Substring(2, 4);
+            Console.WriteLine(A);
+
+        }
+    }
+}
